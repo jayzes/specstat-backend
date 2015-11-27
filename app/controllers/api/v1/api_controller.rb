85b919e462355170d47290dae8bd61d447e85bfa
Jay Zeschin <jay.zeschin@modeset.com>
2015-11-27 18:13:24 -0500

    Hound style fixes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ApiController < ApplicationController
 
   before_action :find_account
-
   respond_to :json
 
   private
 
   def find_account
-    api_token = (request.headers['HTTP_AUTHORIZATION'] || '').gsub(/Bearer(\s*)/, '')
+    api_token = (request.headers["HTTP_AUTHORIZATION"] || "")
+    api_token.gsub!(/Bearer(\s*)/, "")
     unless @account = Account.where(api_token: api_token).first
       head :unauthorized
     end
